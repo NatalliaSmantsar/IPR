d2a02eb50ba6c4f1a860f07fb4dbb895647b5e7c
Natallia Smantsar <user@WIN-A8CN97QOJM8>
2025-04-14 03:31:08 +0300

    deploy to github pages

---
+++ precache-manifest.06954d254f7dceb1e07c8efbed62ed2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6070af0f46b542686b6ea7a356cb9e19",
+    "url": "/IPR/index.html"
+  },
+  {
+    "revision": "d9f46442f704a8c700b7",
+    "url": "/IPR/static/css/main.a0b08e1a.chunk.css"
+  },
+  {
+    "revision": "462d1697d3d114904d7a",
+    "url": "/IPR/static/js/2.1b65383d.chunk.js"
+  },
+  {
+    "revision": "2ad33e9abe21e6769bbc",
+    "url": "/IPR/static/js/3.bd282c0b.chunk.js"
+  },
+  {
+    "revision": "d9f46442f704a8c700b7",
+    "url": "/IPR/static/js/main.ada6903c.chunk.js"
+  },
+  {
+    "revision": "3f408e7c99e7ca138e1b",
+    "url": "/IPR/static/js/runtime~main.ceca91ba.js"
+  }
+]);
